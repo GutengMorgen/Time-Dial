82c5726a03a870f5faca4aa371062b5a4ec67011
guteng morgen <crisstian2345@gmail.com>
2023-11-23 23:51:39 -0500

    add: DataBaseManager

---
+++ src/main/java/com/gutengmorgen/TimeDial/parsing/DataBaseManager.java
@@ -0,0 +1,22 @@
+package com.gutengmorgen.TimeDial.parsing;
+
+import java.util.List;
+
+public class DataBaseManager {
+	private static final String DEFAULTPATH = "src/resources/database/";
+	public static final String TEMPLATE = "templates.db";
+	public static final String DATA_TEMPORAL = "temporal.db";
+	public static final String BOOKMARK = "templates.db";
+	public static final String DELIMITER_MAJOR = ";";
+	public static final String DELIMITER_MINOR = ",";
+	public static final String DELIMITER_TEMPLATE = ":";
+	
+	public static List<String> getData(){
+		return null;
+	}
+	
+	public static void setData(String dbPath) {
+		
+	}
+	
+}
